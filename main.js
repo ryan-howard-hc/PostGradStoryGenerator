387b1533e3857c35107080bead727bec93154069
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-09-25 21:38:17 -0400

    stories generate but dont replace with inserts with proper data. May need to make a few string literal situations for the time being

--- main.js
+++ main.js
@@ -1,75 +1,56 @@
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
-let insertX = [];
-let insertY=[];
-let insertZ=[];
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-
-const storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`;
-
 const urls = [
-    "https://www.jsonkeeper.com/b/VB50",
-    "https://www.jsonkeeper.com/b/PBS1",
-    "https://www.jsonkeeper.com/b/YUS3"
-  ];
-  
-  Promise.all(urls.map(url => fetch(url).then(response => response.json())))
-    .then(dataArray => {
-      const [insertX, insertY, insertZ] = dataArray; 
-      console.log("Data for insertX:", insertX.names);
-      console.log("Data for insertY:", insertY.names);
-      console.log("Data for insertZ:", insertZ.locations);
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    });
+  "https://www.jsonkeeper.com/b/VB50",
+  "https://www.jsonkeeper.com/b/PBS1",
+  "https://www.jsonkeeper.com/b/YUS3",
+  "https://www.jsonkeeper.com/b/6G1M"
+];
 
+Promise.all(urls.map(url => fetch(url).then(response => response.json())))
+  .then(dataArray => {
+    const [insertX, insertY, insertZ, storyTextData] = dataArray; // Add a new variable for the new JSON data
+    console.log("Data for insertX:", insertX.names);
+    console.log("Data for insertY:", insertY.names);
+    console.log("Data for insertZ:", insertZ.locations);
+    console.log("Data for newJsonData:", storyTextData.stories); // Access data from the new JSON file
 
+    randomize.addEventListener('click', result);
 
+    function result() {
+        const newStoryArray = storyTextData.stories; 
+        let selectedStory = randomValueFromArray(newStoryArray);
 
-randomize.addEventListener('click', result);
+      const xItem = randomValueFromArray(insertX.names);
+      const yItem = randomValueFromArray(insertY.names);
+      const zItem = randomValueFromArray(insertZ.locations);
 
-function result() {
-let newStory = storyText;
+      if (customName.value !== '') {
+        const name = customName.value;
+        selectedStory = selectedStory.replace('Bob', name); // Perform replacements on the selected story
+      }
 
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
+      selectedStory = selectedStory.replaceAll(':insertx:', xItem);
+      selectedStory = selectedStory.replace(':inserty:', yItem);
+      selectedStory = selectedStory.replace(':insertz:', zItem);
 
+      if (document.getElementById("uk").checked) {
+        const weight = `${Math.round(300 * 0.0714286)} stone`;
+        const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+        selectedStory = selectedStory.replaceAll('94 Fahrenheit', temperature);
+        selectedStory = selectedStory.replaceAll('300 pounds', weight);
+      }
 
-  if (customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace ('Bob', name);
-  }
-
-  newStory = newStory.replaceAll(':insertx:',xItem);
-  newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':insertz:',zItem);
-
-  console.log (document.getElementById("uk").checked)
-  console.log (`${Math.round(2.112)} stone`)
-  
-//  if(document.getElementById("uk").checked) {
-//    const weight = Math.round.concat(`21.4286, "stone"`);
-//    const temperature =  Math.round.concat(`34.4444, "centigrade"`); }
-  
-   if (document.getElementById("uk").checked) {
-      const weight = `${Math.round(300*0.0714286)} stone`;
-      const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
-      newStory = newStory.replaceAll('94 fahrenheit', temperature);
-      newStory = newStory.replaceAll('300 pounds', weight);
+      story.textContent = selectedStory;
+      story.style.visibility = 'visible';
     }
-    
-  {
-    story.textContent = newStory ;
-    story.style.visibility = 'visible';
-  }
-}
-
-fetchData().then(result);
-randomize.addEventListener('click', result);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
